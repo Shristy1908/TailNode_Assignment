57b3005d537b1249991e0a656e3e102fb649d302
Shristy Kumari <82372346+Shristy1908@users.noreply.github.com>
2023-09-28 10:48:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Form } from "./Components/Form";
+
+function App() {
+
+  return (
+    <div className="wrapper">
+      <h3>Todo List</h3>
+      <div className="todo-box">
+        <Form/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
